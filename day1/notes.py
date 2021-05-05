f67f313c6766f2be0552066d6be5fe52b9d1295e
Diego <dmartinm1995@gmail.com>
2021-05-04 19:30:56 -0700

    Day 1: Working with variables, print() and input() functions

---
+++ day1/notes.py
@@ -0,0 +1,32 @@
+
+# Interactive coding exercise
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# String manipulation
+print("Hello world!\nHello world!\nHello world!")
+
+# String concatenation
+print("Hello" + "Diego")
+
+# Interactive debugging practice
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign')
+print('e.g print("Hello " + "World")')
+print("New lines can be created with a backslash and n.")
+
+# Using the input() function to get data from user and passing it into the print() function
+print("Hello " + input("What is your name? ") + "!")
+
+# Interactive input exercise
+print(len(input("What is your name? ")))
+
+# Using variables to store return statements from input() function
+a = input("a: ")
+b = input("b: ")
+c = b
+b = a
+a = c
+print("a = " + a)
+print("b = " + b)
