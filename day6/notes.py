7c9bf3c9012b35d369020165ddaa39cc2e81ab72
Diego <dmartinm1995@gmail.com>
2021-05-09 18:41:52 -0700

    Functions and While loops

---
+++ day6/notes.py
@@ -0,0 +1,11 @@
+# Functions
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# Using while loops
+
+# Learning about indentations vs spaces
+
+# Used: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json to do the coding exercises
