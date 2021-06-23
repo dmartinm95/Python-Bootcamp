342fd98ed36dd25580d18bf2db6c466ca7d18d79
Diego <dmartinm1995@gmail.com>
2021-06-23 09:58:06 -0700

    Day 24: Updated snake game to store and retrive highscore value from data.txt file

--- day20_21/main.py
+++ day20_21/main.py
@@ -1,7 +1,7 @@
 # Day 20: Snake Game Project
 from scoreboard import Scoreboard
 from snake import Snake
-from turtle import Screen, heading, xcor, ycor
+from turtle import Screen
 from food import Food
 import time
 
@@ -38,14 +38,14 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
--- day20_21/scoreboard.py
+++ day20_21/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("day20_21\data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -18,9 +20,17 @@ class Scoreboard(Turtle):
         self.score += 1
         self.update_scoreboard()
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("day20_21\data.txt", mode="w") as data:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", False,
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False,
                    align=ALIGNMENT, font=FONT)
 
     def game_over(self):
