693ff5f88ddebf2e09835128c06fc58a363c0d16
Diego <dmartinm1995@gmail.com>
2021-10-03 11:05:54 -0700

    Day 39: Flight deal finder

---
+++ day39/main.py
@@ -0,0 +1,3 @@
+# Day 39: Capstone Project - Flight Deal Finder
+
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
